854289bd5bc4bfcf23e4600185a20e537613d27c
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-20 20:32:34 +0530

    updated batch link

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,13 +5,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import React, { useState } from 'react';
 
+function getBaseUrl() {
+	if (typeof window !== "undefined")
+	  return "";
+
+	if (process.env.VERCEL_URL)
+	  return `https://${process.env.VERCEL_URL}`;
+
+	return `http://localhost:${process.env.PORT ?? 3000}`;
+  }
+
 export function Providers({ children }: { children: React.ReactNode }) {
 	const [queryClient] = useState(() => new QueryClient({}));
 	const [trpcClient] = useState(() =>
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					url: 'http://localhost:3000/api/trpc',
+					url: `${getBaseUrl()}/api/trpc`,
 				}),
 			],
 		}),
