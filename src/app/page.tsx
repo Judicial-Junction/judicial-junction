f56308aef3d38edcef0afc1fa128c64654bbc982
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-21 19:01:00 +0530

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { authConfig } from '@/config/auth';
-import { getServerSession } from 'next-auth';
-
-export default async function Home() {
-	const sess = await getServerSession(authConfig);
-
-	if(!sess){
-		return <h1>Not authenticated</h1>
-	}
+import { ThemeSwitcher } from "./_components/ThemeSwitcher";
+export default function Home() {
 
 	return (
 		<>
-			<h1>Authenticated</h1>
+			<ThemeSwitcher />
 		</>
 	);
 }
