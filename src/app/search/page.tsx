694654f0c3dd0f4b92191d47e2a0f30e44afb7b3
Somyeh Virmani <somye01dev@gmail.com>
2023-09-23 16:40:07 +0530

    moved to a new comp

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,21 +1,21 @@
+
+
 import { title } from "@/app/_components/primitives";
 import { Divider } from "@nextui-org/divider";
-import { Textarea } from "@nextui-org/input";
+import SearchText from "./_component/text";
 export default function Search() {
+    
+
+    
+    
     return (
         <section className="flex items-center justify-items-center justify-center mt-20">
-			<div className="max-w-lg text-center justify-center gap-4 mt-28">
-            <h1 className={title({ color: 'violet' })}>Search&nbsp;</h1>
-				<h1 className={title()}>through all</h1>
-                <Divider className="my-4  opacity-0"/>
-                    <Textarea
-                        minRows={1}
-                        color="primary"
-                        variant="bordered"
-                        className="  w-unit-9xl   pr-14  "
-                        size="lg"
-                        />
+            <div className="max-w-lg text-center justify-center gap-4 mt-28">
+                <h1 className={title({ color: 'violet' })}>Search&nbsp;</h1>
+                <h1 className={title()}>Your Queries</h1>
+                <Divider className="my-4 opacity-0" />
+                <SearchText />
             </div>
         </section>
-    )
+    );
 }
