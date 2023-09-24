9e9cfa01b2e0d4745d7d97a93ff10e7f4fc5c0f9
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-24 17:49:41 +0530

    made server

--- src/server/index.ts
+++ src/server/index.ts
@@ -35,12 +35,30 @@ export const appRouter = router({
 		.input(
 			z.object({
 				case_number: z.string(),
-				query: z.string()
-			})
+				query: z.string(),
+			}),
 		)
-		.mutation(async ({input}) => {
-			return 'response message by bot';
-		})
+		.mutation(async ({ input }) => {
+			const myHeaders = new Headers();
+			myHeaders.append('Content-Type', 'application/json');
+
+			const raw = JSON.stringify({
+				query: input.query,
+				case_number: input.case_number,
+			});
+
+			const response = await fetch(
+				'https://chatserver-production-f10b.up.railway.app/query',
+				{
+					method: 'POST',
+					headers: myHeaders,
+					body: raw,
+				},
+			);
+
+			const res = await response.text();
+			return res;
+		}),
 });
 
 export type AppRouter = typeof appRouter;
