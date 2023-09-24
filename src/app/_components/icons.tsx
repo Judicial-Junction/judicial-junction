56867fb880547b439ab4810b98c31713663ab6a2
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-24 15:10:22 +0530

    added elbow icon

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -228,3 +228,18 @@ export const IconArrowRight: React.FC<IconSvgProps> = (props) => {
 		</svg>
 	);
 };
+
+export const IconArrowElbow: React.FC<IconSvgProps> = (props) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 256 256"
+			fill="currentColor"
+			height={24}
+			width={24}
+			{...props}
+		>
+			<path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z" />
+		</svg>
+	);
+};
