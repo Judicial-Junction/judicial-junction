21b3dc763c20d6f7503b491f9816b1c1ec431e91
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-24 10:25:17 +0530

    changed output display

--- src/app/search/_component/text.tsx
+++ src/app/search/_component/text.tsx
@@ -53,26 +53,28 @@ export default function SearchText() {
 				{mut.isSuccess &&
 					mut.data.map((result, index) => (
 						<div key={index} className="flex flex-col">
-							{/* <div className="flex my-2 justify-center">
-								<p className="mr-2 font-bold">
-									Relevance Score 
-								</p>
-								
-							</div>
-							<p className="mr-2">{result._score}</p> */}
 							<div className="flex my-2 justify-center">
-								<p className="mr-2 font-bold">Relevant Cases</p>
+								<p className="mr-2 font-bold">
+									Case Number - {result.fields.case_number[0]}
+								</p>
 							</div>
 
-							<div className="flex my-2" key={index}>
+							<hr className="my-2 invisible" />
+
+							<div className="flex my-2 justify-center">
+								<p className="mr-2 font-bold">Case Name</p>
+							</div>
+
+							<div className="flex my-2 justify-center" key={index}>
 								<p className="font-bold">{index + 1}.</p>
 								<p className="">{result.fields.file_name[0]}</p>
 							</div>
 
 							<hr className="my-2 invisible" />
+							
 							<div className="flex my-2 justify-center">
 								<p className="mr-2 font-bold">
-									Summaries of Cases
+									Case Summary
 								</p>
 							</div>
 
