23bdf4dd4ea87d21939a376d8b154e0fe3be6b52
Tejas013 <tejasmadhukar6@gmail.com>
2023-09-30 20:38:07 +0530

    made dropdown and plot into client

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,28 +1,13 @@
-'use client'
-import { Button } from "@nextui-org/button";
-import { Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from "@nextui-org/dropdown";
-import clsx from "clsx";
-import { title } from "../_components/primitives";
+import clsx from 'clsx';
+import { title } from '../_components/primitives';
+import MainContainer from './_components/main';
 export default function Map() {
 	return (
 		<>
-		<div className="flex flex-col pt-8 items-center ">
-			<h1 className={clsx(" mb-4 text", title())}>Select A City</h1>
-			<Dropdown>
-				<DropdownTrigger>
-					<Button variant="bordered" size="md" color="danger">
-						Choose city
-					</Button>
-				</DropdownTrigger>
-				<DropdownMenu>
-					<DropdownItem key='Delhi'>Delhi</DropdownItem>
-					<DropdownItem key='Hyderabad'>Hyderabad</DropdownItem>
-					<DropdownItem key='Mumbai'>Mumbai</DropdownItem>
-				</DropdownMenu>
-			</Dropdown>
-			
-		</div>
-			
+			<div className="flex flex-col pt-8 items-center ">
+				<h1 className={clsx('mb-4 text-4xl')}>Select A City</h1>
+				<MainContainer />
+			</div>
 		</>
 	);
 }
