0ef0e37bab94cf210d6c83fef4e8b6e3a988d6fb
Tejas013 <tejasmadhukar6@gmail.com>
2023-10-07 15:56:11 +0530

    imported env and changed to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+await import('./src/env.mjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
@@ -11,4 +13,4 @@ const nextConfig = {
 		],
 	},
 };
-module.exports = nextConfig;
+export default nextConfig;
