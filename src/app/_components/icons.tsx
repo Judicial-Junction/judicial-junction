bcdb5e08b44da61bb3139049ceec25ac6d7d2ff8
Tejas013 <tejasmadhukar6@gmail.com>
2023-10-08 22:32:06 +0530

    added an icon

--- src/app/_components/icons.tsx
+++ src/app/_components/icons.tsx
@@ -288,3 +288,31 @@ export const ChatIcon: React.FC<IconSvgProps> = (props) => {
 		</svg>
 	);
 };
+
+export const ChevronDown: React.FC<IconSvgProps> = ({
+	fill,
+	size,
+	height,
+	width,
+	...props
+}) => {
+	return (
+		<svg
+			fill="currentColor"
+			height={size || height || 18}
+			viewBox="0 0 24 24"
+			width={size || width || 18}
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path
+				d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
+				stroke={fill}
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeMiterlimit={10}
+				strokeWidth={1.5}
+			/>
+		</svg>
+	);
+};
