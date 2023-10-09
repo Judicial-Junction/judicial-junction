ad12612586f94cc8f076144c2934d4d2085df046
Tejas013 <tejasmadhukar6@gmail.com>
2023-10-09 10:04:46 +0530

    added user avatar dropdown

--- src/app/_components/Navbar/LogoutButton.tsx
+++ src/app/_components/Navbar/LogoutButton.tsx
@@ -1,17 +1,46 @@
 'use client';
-import { Button } from '@nextui-org/button';
-import { signOut } from 'next-auth/react';
-import { RedUserIcon } from '../icons';
+import { Avatar } from '@nextui-org/avatar';
+import {
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
+} from '@nextui-org/dropdown';
+import { signOut, useSession } from 'next-auth/react';
 
-export function LogoutButton() {
+const LogoutButton = () => {
+	const session = useSession();
 	return (
-		<Button
-			startContent={<RedUserIcon />}
-			onPress={() => signOut()}
-			variant="flat"
-			color="danger"
-		>
-			Logout
-		</Button>
+		<Dropdown>
+			<DropdownTrigger>
+				<Avatar
+					name={session.data?.user.name ?? 'caesar'}
+					src={
+						session.data?.user.image ??
+						'https://i.pravatar.cc/150?u=a042581f4e29026704d'
+					}
+				/>
+			</DropdownTrigger>
+			<DropdownMenu aria-label="Profile Actions" variant="flat">
+				<DropdownItem key="profile" className="h-14 gap-2">
+					<p className="font-semibold">Signed in as</p>
+					<p className="font-semibold">
+						{session.data?.user.email ??
+							'cannot retrieve your email'}
+					</p>
+				</DropdownItem>
+				<DropdownItem key="settings">My Settings</DropdownItem>
+
+				<DropdownItem
+					key="logout"
+					onPress={() => signOut()}
+					color="danger"
+				>
+					Log Out
+				</DropdownItem>
+			</DropdownMenu>
+		</Dropdown>
 	);
-}
+};
+
+export default LogoutButton;
