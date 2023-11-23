2d7bb4ba20720f48e34634564e278529812aca20
Tejas013 <tejasmadhukar6@gmail.com>
2023-11-23 23:22:26 +0530

    removed get started button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,14 @@
-import { Link } from '@nextui-org/link';
-import { button as buttonStyles } from '@nextui-org/theme';
 import TextAnimation from './_components/Landing/text';
 
 export default function Home() {
-	const customStyles = {
-		fontSize: '16px',
-		width: '180px',
-		height: '60px',
-	};
 	return (
 		<>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 justify-items-center">
-				<div className=" mt-40 text-[19px]">
+				<div className="mt-40 text-[19px]">
 					<TextAnimation />
 				</div>
 				<div className="flex gap-3 mt-8 ">
-					<Link
-						href={'/search'}
-						className={buttonStyles({
-							color: 'default',
-							radius: 'full',
-							variant: 'ghost',
-						})}
-						style={customStyles}
-					>
-						Get Started
-					</Link>
+					<h1 className=" text-default">Our services</h1>
 				</div>
 			</section>
 		</>
