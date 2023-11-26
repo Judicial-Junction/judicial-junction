9f9e733e19bf07d639d461aee2249427eb749700
Tejas013 <tejasmadhukar6@gmail.com>
2023-11-26 15:50:38 +0530

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,47 @@
+import clsx from 'clsx';
+import FeaturesDropdownLanding from './_components/Landing/FeaturesDropdownLanding';
+import GreetingText from './_components/Landing/GreetingText';
+import ImageCard from './_components/Landing/ImageCard';
 import TextAnimation from './_components/Landing/text';
-
+import { subtitle, title } from './_components/primitives';
 export default function Home() {
 	return (
 		<>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 justify-items-center">
-				<div className="mt-40 text-[19px]">
+				<div className="mt-40 hidden sm:flex text-[19px]">
 					<TextAnimation />
 				</div>
-				<div className="flex gap-3 mt-8 ">
-					<h1 className=" text-default">Our services</h1>
+				<div className="flex flex-col gap-3 mt-8 ">
+					<GreetingText />
+					<div className="sm:hidden mx-auto mt-6">
+						<FeaturesDropdownLanding />
+					</div>
+					<div className=" mt-14 mx-auto hidden flex-col">
+						<h1
+							className={clsx(
+								subtitle(),
+								'font-bold text-xl text-center',
+							)}
+						>
+							Our services
+						</h1>
+						<div className="mt-16 flex gap-24">
+							<ImageCard />
+						</div>
+					</div>
+					<div className="mt-14 mx-auto sm:hidden flex  flex-col">
+						<h1
+							className={clsx(
+								subtitle(),
+								'font-bold text-xl text-center',
+							)}
+						>
+							Our services
+						</h1>
+						<div className="mt-6">
+							<ImageCard />
+						</div>
+					</div>
 				</div>
 			</section>
 		</>
