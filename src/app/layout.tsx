abe7d0859a1891e0f73072f81847c3456cd66bd3
Tejas013 <tejasmadhukar6@gmail.com>
2024-01-31 12:02:59 +0530

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,41 +5,43 @@ import clsx from 'clsx';
 import type { Metadata } from 'next';
 import Navbar from './_components/Navbar/navbar';
 import { Providers } from './providers';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	icons: {
-		icon: '/favicon.ico',
-		shortcut: '/favicon-16x16.png',
-		apple: '/apple-touch-icon.png',
-	},
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<head />
-			<body
-				className={clsx(
-					'min-h-screen bg-background font-sans antialiased',
-					fontSans.variable,
-				)}
-			>
-				<Providers>
-					<div className="min-h-screen bg-gradient-to-b from-primary flex flex-col">
-						<Navbar />
-						{children}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head />
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        <Providers>
+          <div className="min-h-screen bg-gradient-to-b from-primary flex flex-col">
+            <Navbar />
+            {children}
+          </div>
+        </Providers>
+        <Analytics />
+      </body>
+    </html>
+  );
 }
