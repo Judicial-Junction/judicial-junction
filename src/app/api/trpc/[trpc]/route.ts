ecbada75b2bc4777271f72565276f97c74d819c1
Tejas013 <tejasmadhukar6@gmail.com>
2024-02-18 12:30:36 +0530

    changed name removed next-auth import and created empty context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,20 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import {
+  FetchCreateContextFnOptions,
+  fetchRequestHandler,
+} from '@trpc/server/adapters/fetch';
 
 import { appRouter } from '@/server/api';
 
 const handler = (req: Request) =>
-	fetchRequestHandler({
-		endpoint: '/api/trpc',
-		req,
-		router: appRouter,
-	});
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    createContext: function(
+      opts: FetchCreateContextFnOptions,
+    ): object | Promise<object> {
+      return {};
+    },
+  });
 
 export { handler as GET, handler as POST };
