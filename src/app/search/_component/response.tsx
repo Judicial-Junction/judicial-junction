92150463a3dc0e7de3d170f6e58f5da81e003229
Tejas013 <tejasmadhukar6@gmail.com>
2024-03-29 13:58:17 +0530

    Made ui like tv sir

--- src/app/search/_component/response.tsx
+++ src/app/search/_component/response.tsx
@@ -1,6 +1,4 @@
-import { DocIcon, FileIcon } from "@/app/_components/icons";
 import { SearchResponse } from "@/server/api/routers/search_utils";
-import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { closest } from "fastest-levenshtein";
 import { useMemo } from "react";
@@ -33,47 +31,65 @@ function GetFuzzySentence(keyword: string, judgement_text: string) {
 
 export default function Response({ data }: { data: SearchResponse[] }) {
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col space-y-2">
       {data.map((result) => (
-        <div className="w-[800px]" key={result._id}>
-          <Link
-            className="blue-500 border-2 border-black text-blue-500"
-            href={result.fields["Judgement PDF URL"][0]}
-          >
-            click here
-          </Link>
-          <h1 className="border-2 border-black bg-blue-100">
-            {result.fields["Case Title"][0]}
-          </h1>
-          {result.fields["Judgement Text"] && (
-            <>
-              {result.search_type == "Fuzzy Search" && (
-                <div className="border-2 border-black">
-                  <p>
-                    {"..." +
+        <div
+          className="center w-[1000px] divide-y divide-slate-400 border-1 border-slate-500 shadow-lg"
+          key={result._id}
+        >
+          <div>
+            <Link
+              className="blue-500 te-200 w-[1000px] bg-indigo-100 text-blue-500"
+              target="_blank"
+              href={result.fields["Judgement PDF URL"][0]}
+            >
+              click here
+            </Link>
+          </div>
+          <div>
+            <h1 className="flex items-start bg-indigo-100 text-xl font-bold">
+              {result.fields["Case Title"][0]}
+            </h1>
+          </div>
+          {result.fields["Judgement Text"] &&
+            result.search_type == "Fuzzy Search" && (
+              <div className="text-left">
+                <p>
+                  {"..." +
+                    GetFuzzySentence(
+                      result.search_query,
+                      result.fields["Judgement Text"][0],
+                    )[0] +
+                    " "}
+                  <mark className="bg-slate-900 text-white">
+                    {
                       GetFuzzySentence(
                         result.search_query,
                         result.fields["Judgement Text"][0],
-                      )[0] +
-                      " "}
-                    <mark className="bg-slate-300 dark:bg-slate-300">
-                      {
-                        GetFuzzySentence(
-                          result.search_query,
-                          result.fields["Judgement Text"][0],
-                        )[1]
-                      }
-                    </mark>
-                    {" " +
-                      GetFuzzySentence(
-                        result.search_query,
-                        result.fields["Judgement Text"][0],
-                      )[2] +
-                      "..."}
-                  </p>
-                </div>
-              )}
-            </>
+                      )[1]
+                    }
+                  </mark>
+                  {" " +
+                    GetFuzzySentence(
+                      result.search_query,
+                      result.fields["Judgement Text"][0],
+                    )[2] +
+                    "..."}
+                </p>
+              </div>
+            )}
+          {result.fields["Sentences"] && (
+            <div className="text-left">
+              <p>
+                {result.fields.Sentences[0] + " "}
+                {result.fields.Sentences[1] + " "}
+                <mark className="bg-slate-900 text-white">
+                  {result.fields.Sentences[2] + " "}
+                </mark>
+                {result.fields.Sentences[3] + " "}
+                {result.fields.Sentences[4]}
+              </p>
+            </div>
           )}
         </div>
       ))}
