d3ace64b668a3368663bb671d0bde478862e9005
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-10 00:44:05 +0530

    Made chat endpoint

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -1,6 +1,8 @@
 import { env } from "@/env.mjs";
 import { createTRPCRouter, publicProcedure } from "@/server/trpc";
+import { TRPCError } from "@trpc/server";
 import OpenAi from "openai";
+import { z } from "zod";
 
 const openai = new OpenAi({
   apiKey: env.OPENAI_API_KEY,
@@ -20,4 +22,26 @@ export const openai_router = createTRPCRouter({
 
     return response.choices[0].message.content;
   }),
+  ChatWithCase: publicProcedure
+    .input(
+      z.array(
+        z.object({ role: z.enum(["system", "user"]), content: z.string() }),
+      ),
+    )
+    .query(async ({ input }) => {
+      try {
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: input,
+        });
+
+        return response.choices[0].message.content;
+      } catch (error) {
+        console.log(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "problem with openai chat endpoint",
+        });
+      }
+    }),
 });
