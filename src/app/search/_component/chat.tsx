62ba3f44c21c3b4a4a07192875417597f5a495e9
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-09 23:39:33 +0530

    Chat ui done

--- src/app/search/_component/chat.tsx
+++ src/app/search/_component/chat.tsx
@@ -1,12 +1,15 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogOverlay,
 } from "@/components/ui/dialog";
-import { useRef, type FC, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRef, type FC, useState, type FormEvent } from "react";
+import clsx from "clsx";
 
 enum Creator {
   System,
@@ -27,39 +30,62 @@ interface Message {
 const ChatDialog: FC<ChatDialogProps> = () => {
   const bottomRef = useRef<HTMLDivElement>(null);
   const [messages, setmessages] = useState<Message[]>([
-    { owner: Creator.System, text: "Ask your questions about this case" },
+    { owner: Creator.System, text: "Ask questions about the ask !!" },
   ]);
+  const [input, setinput] = useState("");
+
+  const HandleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (input === "") return;
+    setmessages((prev) => [...prev, { owner: Creator.User, text: input }]);
+    setinput("");
+  };
+
   return (
     <Dialog>
       <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Chat with Case</DialogTitle>
-          <DialogDescription>ask question about cases</DialogDescription>
-          {/*Input here */}
-          <div className="full flex flex-col">
-            <div className="flex-1 overflow-y-auto">
-              {/*Messages here*/}
-              {messages.map((message, idx) => (
-                <div key={idx}>
-                  {message.owner == Creator.System ? (
-                    <div>
-                      <h3>System</h3>
+      <DialogOverlay className="">
+        <DialogContent className="">
+          <DialogHeader>
+            <DialogTitle>Chat with Case</DialogTitle>
+            {/*Input here */}
+            <div className="flex h-[400px] flex-col">
+              <div className="flex-1 space-y-2 overflow-y-auto text-center">
+                {messages.map((message, idx) => (
+                  <div
+                    key={idx}
+                    className={clsx(
+                      message.owner == Creator.System
+                        ? "bg-slate-100 text-left"
+                        : "text-right",
+                      "border-b-1 border-slate-200",
+                    )}
+                  >
+                    {message.owner == Creator.System ? (
                       <p>{message.text}</p>
-                    </div>
-                  ) : (
-                    <div>
-                      <h3>User</h3>
+                    ) : (
                       <p>{message.text}</p>
-                    </div>
-                  )}
-                </div>
-              ))}
-              <div ref={bottomRef} />
+                    )}
+                  </div>
+                ))}
+                <div ref={bottomRef} />
+              </div>
+              <form
+                className="mt-2 flex w-full max-w-sm items-center space-x-2"
+                onSubmit={HandleSubmit}
+              >
+                <Input
+                  type="search"
+                  value={input}
+                  onChange={(e) => setinput(e.target.value)}
+                  placeholder="chat"
+                />
+                <Button type="submit">Send</Button>
+              </form>
             </div>
-          </div>
-        </DialogHeader>
-      </DialogContent>
+          </DialogHeader>
+        </DialogContent>
+      </DialogOverlay>
     </Dialog>
   );
 };
